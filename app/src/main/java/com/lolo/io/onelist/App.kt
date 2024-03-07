334c75dda46a124a79a0229ad423726708546e71
Paul D'Ambra <paul@posthog.com>
2024-03-07 08:50:27 +0000

    get it running with posthog replay

--- app/src/main/java/com/lolo/io/onelist/App.kt
+++ app/src/main/java/com/lolo/io/onelist/App.kt
@@ -4,14 +4,33 @@ import android.app.Application
 import com.lolo.io.onelist.core.data.di.appModule
 import com.lolo.io.onelist.feature.lists.di.listsModule
 import com.lolo.io.onelist.feature.settings.di.settingsModule
+import com.posthog.android.PostHogAndroid
+import com.posthog.android.PostHogAndroidConfig
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.fragment.koin.fragmentFactory
 import org.koin.core.context.startKoin
 
 class App : Application() {
 
+
+    companion object {
+        const val POSTHOG_API_KEY = "phc_pQ70jJhZKHRvDIL5ruOErnPy6xiAiWCqlL4ayELj4X8"
+    }
+
     override fun onCreate() {
         super.onCreate()
+
+        val config = PostHogAndroidConfig(POSTHOG_API_KEY).apply {
+            // sessionReplay is disabled by default
+            sessionReplay = true
+            // sessionReplayConfig is optional, they are enabled by default
+            sessionReplayConfig.maskAllTextInputs = true
+            sessionReplayConfig.maskAllImages = false
+            sessionReplayConfig.captureLogcat = true
+            debug=true
+        }
+        PostHogAndroid.setup(applicationContext, config)
+
         startKoin {
             androidContext(this@App)
             fragmentFactory()
